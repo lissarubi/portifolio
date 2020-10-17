d214bd17b1264c7fb0acd6b03b6115a86127a4c3
Ederson <edersondeveloper@gmail.com>
2020-10-17 18:26:37 -0400

    moving to dist

---
+++ dist/src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <a :style="style" :href="link" class="tech" target="_blank">{{ name }}</a>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    color: String,
+    name: String,
+    link: String,
+  },
+  computed: {
+    style() {
+      return 'background-color: #' + this.color;
+    },
+  },
+};
+</script>
+
+<style>
+@import '../assets/css/button.css';
+</style>
